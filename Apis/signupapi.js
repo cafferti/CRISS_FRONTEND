d0ccfef7635da1b13856d496af44ea7c212e063b
cafferti <saulkumven@gmail.com>
2023-08-28 11:54:25 +0100

    started api consumption

---
+++ Apis/signupapi.js
@@ -0,0 +1,11 @@
+const getSignUpInput = () =>{
+    const Email = document.getElementById(`email`).value;
+    const Rank = document.getElementById(`rank`).value;
+    const Station = document.getElementById(`station`).value;
+    const Password = document.getElementById(`password`).value;
+    const ConfirmPassword = document.getElementById(``).value
+
+
+
+}
+
